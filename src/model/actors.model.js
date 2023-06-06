61a6c3001bcf50bbfcaddf369c891814eb8ae061
RanjithKumar <106184000+Ranjithp123-hash@users.noreply.github.com>
2023-06-06 18:01:50 +0530

    Delete src directory

--- src/model/actors.model.js
+++
@@ -1,9 +0,0 @@
-const mongoose = require("mongoose");
-
-const actorSchema = mongoose.Schema({
-    actorname:{type:String, required: true}
-})
-
-const actor = mongoose.model("movies", actorSchema);
-
-module.exports = actor;
